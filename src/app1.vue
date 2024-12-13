835a65b44796496788368d83ab94868b2e9fe4d5
ongkenith <ongkenith@gmail.com>
2024-12-13 22:13:33 +0800

    Update app1.vue
    changes to make it more mobile friendly

--- src/app1.vue
+++ src/app1.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import playInfoModal from "@/components/howToPlayModal.vue"
+
 </script>
 
 <template>
@@ -7,24 +7,19 @@
     <h2 class="my-2">2048<br>The Remake</h2>
     <div class="container">
       <div class="row mt-4 justify-content-center">
-        <div class="col-2">
+        <div class="col-md-2 my-2">
           <a href="src/classic.html"><button type="button" class="btn gameButton">Classic Mode</button></a>
         </div>
-        <div class="col-2">
+        <div class="col-md-2 my-2">
           <a href="src/limitless.html"><button type="button" class="btn gameButton">Limitless Mode</button></a>
         </div>
-        <div class="col-2">
+        <div class="col-md-2 my-2">
           <a><button disabled='true' type="button" class="btn gameButton">Custom Mode</button></a>
         </div>
-        <div class="col-12 mt-3">
-          <button type="button" class="btn gameButton" data-bs-toggle="modal" data-bs-target="#howToPlay">How to play?</button>
-        </div>
+        <div class="col-md-2 my-1">*If color looks unfriendly, play in light mode settings*</div>
       </div>
     </div>
   </div>
-
-  <!-- Info Modal -->
-  <playInfoModal></playInfoModal>
 </template>
 
 <style scoped>
