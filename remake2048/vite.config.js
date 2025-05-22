a1f1bd013edceafa15539f041bbf512b56a9c046
ongkenith <ongkenith@gmail.com>
2025-05-22 23:23:43 +0800

    Update vite.config.js

--- remake2048/vite.config.js
+++ remake2048/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./remake2048/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   base: "/projects/remake2048/",
